8f28326c448b464305896b599860fa31c2bcc9a9
Guruprasad <guruprasadgaikwad8@gmail.com>
2023-02-15 08:09:15 +0530

    Topbar component is created and designed using vanilla css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import Home from  './pages/Home'
+import Home from  './pages/home/Home'
 
 function App() {
-  return <Home />
+  return <Home/>
 }
 
 export default App;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,9 @@
+import Topbar from "../../components/topbar/Topbar"
+
 export default function Home() {
   return (
     <div>
-      This is Home
+      <Topbar />
     </div>
   )
 }
